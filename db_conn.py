c228b0bf13856aaa4c443b122145d7eb43bfbe30
marcus overhue <marcusoverheu@gmail.com>
2011-03-18 15:29:22 +0100

    db_conn adds rows to text table

--- db_conn.py
+++ db_conn.py
@@ -5,21 +5,20 @@ import psycopg2
 import sys
 import csv
 try:
-    reader = csv.reader(open('data/UN_nuclear_el.csv', 'rb'))
+    reader = csv.reader(open('un/elec_nuclear.csv', 'rb'))
 
-    for row in reader:
-        if reader.line_num != 0 and len(row) == 6:
-            print(len(row))
+
 
     conn = psycopg2.connect("dbname = 'db1' user = 'maov' host='localhost' password='bajaro22'")
     cur = conn.cursor()
-    cur.execute("""SELECT count(*) FROM users""")
-    
-    rows = cur.fetchall()
-    print("row count")
-    for row in rows:
-        print(" ", row[0])
-        print("length: ", len(row)) 
+
+    for row in reader:
+        if reader.line_num == 0:
+              continue
+        elif len(row) == 6:
+            print("called alot")
+            cur.execute("INSERT INTO nuclear VALUES(%s, %s, %s, %s, %s, %s)", row)
+        conn.commit()
 except:
     # Get the most recent exception
     exceptionType, exceptionValue, exceptionTraceback = sys.exc_info()
